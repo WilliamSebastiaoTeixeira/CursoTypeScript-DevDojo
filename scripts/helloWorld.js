fb871e1f7753a5f6156e7ed75416cdf03d256a1f
william <williamsebastiaoteixeira@gmail.com>
2022-02-22 23:47:35 -0300

    Some Details

---
+++ scripts/helloWorld.js
@@ -0,0 +1,6 @@
+"use strict";
+var p = document.createElement('p');
+var ola = "Olá Mundo";
+p.textContent = ola;
+document.body.appendChild(p);
+//# sourceMappingURL=helloWorld.js.map
---
+++ scripts/helloWorld.ts
@@ -0,0 +1,4 @@
+var p = document.createElement('p'); 
+var ola : string = "Olá Mundo"; 
+p.textContent = ola; 
+document.body.appendChild(p); 
